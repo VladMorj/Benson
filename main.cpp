7672fcb69ae1420be76e2b5e39318844746677ad
myprojcts <135384959+myprojcts@users.noreply.github.com>
2023-07-14 19:17:25 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,165 @@
+#include <iostream>
+#include <cstdlib>
+#include <locale.h>
+#include <cmath>
+
+using namespace std;
+
+///dH
+void alkan_dH(int m[][5], int stop) ///функция принимает указатель на составленный по введенным данным двумерный массив и количество молекул для рассчета и рассчитывает энтальпийный вклад в свойства для алканов
+{
+    int num = 1;
+    double c[5]={-42.19, -20.64, -7.95, 2.09, 3.35}; /// Парциальный вклад 1 вида взаимодействия
+    cout << "Энтальпия образования (dHf, кДж/моль)" << endl;  ///Суммарный вклад в энтальпия образования
+    for(int i = 0; i<stop; ++i){
+        double *sum = new double(0);  ///выделение памяти для переменной - счетчика (будет освобождена на каждой итерации внешнего цикла)
+        double *pc = new double[5];   ///выделение памяти под массив куда будет записан вклад каждого вида взаимодействий (массив будет освобожден из кучи и определен заново на каждой итерации внешнго цикла)
+        for(int j =0; j < 5; ++j){
+            pc[j]=((m[i][j]) * (c[j]));
+            *sum = *sum + pc[j];
+        }
+        cout << num << ". dHf = " << *sum << "кДж/моль" << endl;
+        num++;
+        delete[]pc;
+        delete sum;
+    }
+}
+
+///dH
+void cyclo_dH(unsigned *m)
+{
+    double sum = 0;
+    double c[5]={-42.19, -20.64, -7.95, 3.35, 67.48};
+    double pc[5];
+    cout << "Enthalpy of formation (dHf,kDj/mol)= ";
+    for(int i = 0; i<5; ++i){
+        pc[i]=((m[i])*(c[i]));
+        sum = sum + pc[i];
+        if(i == 4){
+            cout <<pc[i] << " = ";
+        }
+        else{
+            cout <<pc[i] << " + ";
+        }
+    }
+    cout << sum << endl;
+}
+
+///dH
+void alken_dH(unsigned *m)
+{
+    double sum = 0;
+    double c[7]={-20.64, -7.95, 35.96, 67.48, 4.184, 3.35, 2.09};
+    double pc[7];
+    cout << "Enthalpy of formation (dHf,kDj/mol)= ";
+    for(int i = 0; i<7; ++i){
+        pc[i]=((m[i])*(c[i]));
+        sum = sum + pc[i];
+        if(i == 6){
+            cout <<pc[i] << " = ";
+        }
+        else{
+            cout <<pc[i] << " + ";
+        }
+    }
+    cout << sum << endl;
+}
+
+///dS
+void alkan_dS(unsigned *m, double ext_corr, double int_corr)
+{
+    double pre_simm_corr = 0;
+    double simm_corr = 0;
+    double sum = 0;
+    double R = 8.314;
+    double c1[4]={127.24, 39.41, -50.50, -146.86};
+    double pc[4];
+    cout << "Энтропия образования (dSf,Дж/моль*К)= ";
+    for(int i = 0; i<4; ++i){
+        pc[i]=((m[i]) * (c1[i]));
+        sum = sum + pc[i];
+        if(i == 3){
+            cout <<pc[i] << " = ";
+        }
+        else{
+            cout << pc[i] << " + ";
+        }
+    }
+    cout << sum << " " "Дж/моль*К" <<endl;
+    if(ext_corr==0){
+        pre_simm_corr = int_corr;
+        cout << endl << "Поправка на симметрию молекулы (Дж/моль*К):" << -R*(log(pre_simm_corr)) << endl;
+        simm_corr = -R*(log(pre_simm_corr));
+        cout << endl << "Результирующая энтропия (Дж/моль*К):" << sum+simm_corr << endl;
+    }
+    else if(int_corr==0){
+        pre_simm_corr = ext_corr;
+        cout << endl << "Поправка на симметрию молекулы (Дж/моль*К):" << -R*(log(pre_simm_corr)) << endl;
+        simm_corr = -R*(log(pre_simm_corr));
+        cout << endl << "Результирующая энтропия (Дж/моль*К):" << sum+simm_corr << endl;
+    }
+    else{
+        pre_simm_corr = ext_corr*int_corr;
+        cout << endl << "Поправка на симметрию молекулы (Дж/моль*К):" << -R*(log(pre_simm_corr)) << endl;
+        simm_corr = (-R*(log(pre_simm_corr)));
+        cout << endl << "Результирующая энтропия (Дж/моль*К):" << sum+simm_corr << endl;
+    }
+}
+///dS
+void cyclo_dS(unsigned *m, double ext_corr, double int_corr)
+{
+    double pre_simm_corr = 0;
+    double simm_corr = 0;
+    double sum = 0;
+    double R = 8.314;
+    double c1[3]={127.24, 39.41, -50.50};
+    double pc[4];
+    cout << "Энтропия образования (dSf,Дж/моль*К)= ";
+    for(int i = 0; i<3; ++i){
+        pc[i]=((m[i]) * (c1[i]));
+        sum = sum + pc[i];
+        if(i == 2){
+            cout <<pc[i] << " = ";
+        }
+        else{
+            cout << pc[i] << " + ";
+        }
+    }
+    cout << sum << " " "Дж/моль*К" <<endl;
+    if(ext_corr==0){
+        pre_simm_corr = int_corr;
+        cout << endl << "Поправка на симметрию молекулы (Дж/моль*К):" << -R*(log(pre_simm_corr)) << endl;
+        simm_corr = -R*(log(pre_simm_corr));
+        cout << endl << "Результирующая энтропия (Дж/моль*К):" << sum+simm_corr << endl;
+    }
+    else if(int_corr==0){
+        pre_simm_corr = ext_corr;
+        cout << endl << "Поправка на симметрию молекулы (Дж/моль*К):" << -R*(log(pre_simm_corr)) << endl;
+        simm_corr = -R*(log(pre_simm_corr));
+        cout << endl << "Результирующая энтропия (Дж/моль*К):" << sum+simm_corr << endl;
+    }
+    else{
+        pre_simm_corr = ext_corr*int_corr;
+        cout << endl << "Поправка на симметрию молекулы (Дж/моль*К):" << -R*(log(pre_simm_corr)) << endl;
+        simm_corr = (-R*(log(pre_simm_corr)));
+        cout << endl << "Результирующая энтропия (Дж/моль*К):" << sum+simm_corr << endl;
+    }
+}
+
+
+int main()
+{
+    setlocale(0, "Russian");
+
+    int b = 0;
+    double x = 0;
+    double n = 0;
+    double x1 = 0;
+    double n1 = 0;
+    double ext_corr = 0;
+    double int_corr = 0;
+    cout << "\t" "(используйте цифры 1, 2, 3.. для управления)" << endl << endl <<"Выберите опцию:" << endl;
+    cout << "1. Расчет стандартной энтальпии образования (dHf, кДж/моль);" << endl << "2. Расчет стандартной энтропии образования (dSf, Дж/моль*К);" << endl << "3. Расчет теплоемкости (C, Dj/K)" << endl;
     cin >> b;
     if(b == 1){
         int a = 0;
